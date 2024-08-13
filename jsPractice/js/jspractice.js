cd0b3611a147e299f3b69bc6db16c8dccc410a28
accleto11 <almacleto23@gmail.com>
2024-08-14 01:57:42 +0800

    add minigames folder

---
+++ jsPractice/js/jspractice.js
@@ -0,0 +1,50 @@
+// alert("hello");
+
+// const incrementBtn = document.getElementById("increment");
+// const decrementBtn = document.getElementById("decrement");
+// const resetBtn = document.getElementById("resetBtn");
+// const resultBox = document.getElementById("resultBox");
+// let count = 0;
+
+// function increment() {
+//     count++;
+//     resultBox.textContent = count;
+// }
+
+// function decrement() {
+//     count--;
+//     resultBox.textContent = count;
+// }
+
+// function resetIncrement() {
+//     count = 0;
+//     resultBox.textContent = count;
+// }
+
+// incrementBtn.addEventListener('click', increment);
+// decrementBtn.addEventListener('click', decrement);
+// resetBtn.addEventListener('click', resetIncrement);
+
+// concatenation and template literal
+
+// const fName = "Em";
+
+// console.log("hello " + fName);
+// console.log(`hello ${fName}`);
+
+/**
+ ternary operator
+ condition ? true : false;
+**/
+
+// let printStatus;
+// const age = 16;
+
+// printStatus = (age >= 18) ? "Adult" : "Minor";
+
+// console.log(printStatus);
+
+// example 2
+
+
+
