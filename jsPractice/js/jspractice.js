2de907814ffa9ebd739e0139518ad146329be4b7
accleto11 <almacleto23@gmail.com>
2024-08-17 13:42:34 +0800

    updated minigames

--- jsPractice/js/jspractice.js
+++ jsPractice/js/jspractice.js
@@ -117,56 +117,235 @@ const resetBtn = document.getElementById("resetBtn");
 
 //ternary condition
 
-function age() {
-    const inputBox = parseInt(document.getElementById("inputBox").value);
+// function age() {
+//     const inputBox = parseInt(document.getElementById("inputBox").value);
 
-    inputBox >= 18 ? showResult.textContent = `Welcome, you can enter now. (${inputBox}).` : showResult.textContent = `Sorry, you're still a minor (${inputBox}).`
+//     inputBox >= 18 ? showResult.textContent = `Welcome, you can enter now. (${inputBox}).` : showResult.textContent = `Sorry, you're still a minor (${inputBox}).`
 
-}
+// }
 
-enterBtn.addEventListener('click', age);
+// enterBtn.addEventListener('click', age);
 
-function resetAge() {
-    const inputBox = document.getElementById("inputBox");
-    const showResult = document.getElementById("showResult");
+// function resetAge() {
+//     const inputBox = document.getElementById("inputBox");
+//     const showResult = document.getElementById("showResult");
 
-    inputBox.value = "";
-    showResult.textContent = "";
-}
-resetBtn.addEventListener('click', resetAge);
+//     inputBox.value = "";
+//     showResult.textContent = "";
+// }
+// resetBtn.addEventListener('click', resetAge);
 
 
 
-const correctAnswer = "Friday";
+// const correctAnswer = "Friday";
 
-switch(correctAnswer) {
-    case "Monday":
-        console.log("Hello, Monday");
-        break;
+// switch(correctAnswer) {
+//     case "Monday":
+//         console.log("Hello, Monday");
+//         break;
 
-    case "Wednesday": 
-        console.log("Hello, Wednesday");
-        break;
+//     case "Wednesday": 
+//         console.log("Hello, Wednesday");
+//         break;
     
-    case "Friday":
-        console.log("Correct! Friday!");
-        break;
+//     case "Friday":
+//         console.log("Correct! Friday!");
+//         break;
 
-    default:
-        console.log("day not found");
+//     default:
+//         console.log("day not found");
+// }
+
+
+
+
+// // loops
+
+// let i = 10;
+
+// while (i < 10) 
+// {
+//     i++;
+//     console.log(i);
+// }
+
+// console.log(i);
+
+
+
+const inputBoxLoop = document.getElementById('inputBoxLoop');
+const enterBtnLoop = document.getElementById('enterBtnLoop');
+const resetBtnLoop = document.getElementById('resetBtnLoop');
+const showResultLoop = document.getElementById('showResultLoop');
+
+// while loop
+
+// function guessName() {
+//     const guessName = "Kero";
+//     let whileName = inputBoxLoop.value;
+
+//     while (guessName !== whileName) {
+//     showResultLoop.textContent = "Please try again!";
+//     return; //to exit the function and avoid indifinite looping!
+// };
+
+//     //if the names match / user input the correct name;
+//     showResultLoop.textContent = "Congratulations! You guessed it right!"; 
+// };
+
+// enterBtnLoop.addEventListener('click', guessName);
+
+// // Reset functionality
+// resetBtnLoop.addEventListener('click', () => {
+//     inputBoxLoop.value = '';
+//     showResultLoop.textContent = '';
+// });
+
+//guess random names
+
+// const namesArray = ["Emem", "Kero", "John", "Naneng", "Jane"];
+
+// //randomize the names
+// let randomName = namesArray[Math.floor(Math.random() * namesArray.length)];
+
+// function guessName() {
+//     let userGuess = inputBoxLoop.value;
+
+//     if (userGuess === randomName) {
+//         showResultLoop.textContent = `Correct Anwer: ${randomName}. Congratulations! You guessed it right! `;
+//     } else {
+//         showResultLoop.textContent = "Please try again!";
+//         console.log(randomName);
+//     }
+// };
+
+// enterBtnLoop.addEventListener('click', guessName);
+
+// // Reset functionality
+// resetBtnLoop.addEventListener('click', () => {
+//     inputBoxLoop.value = '';
+//     showResultLoop.textContent = '';
+//     randomName = namesArray[Math.floor(Math.random() * namesArray.length)]; // Pick a new random name
+// });
+
+
+//while loop (check the condition first before processing)
+
+// let num = 0;
+
+// while (num < 10) {
+//     num++;
+//     console.log(num);
+// };
+
+//1-10
+
+// while (num < 10) {
+//     console.log(num);
+//     num++;
+// };
+
+// //0-9
+
+// let num = 9;
+
+// while (num < 10) {
+//     console.log(num);
+//     num++;
+// };
+
+/** num = 9
+    log 9,
+    9 + 1 = 10
+    final log = 9
+**/
+
+//do while loop (process first before checking the condition)
+//don't use when user needs to click button/ enter data in gui
+
+// num = 10;
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while(num < 10);
+
+
+
+//sorting algorithm
+
+//bubble sort
+
+// function bubbleSort(arr) {
+//     let n = arr.length;
+//     for (let i = 0; i < n; i++) {
+//         let swapped = false;
+//         for (let j = 0; j < n - i - 1; j++) {
+//             if (arr[j] > arr[j + 1]) {
+//                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//                 swapped = true;
+//             }
+//         }
+//         if (!swapped) break;
+//     }
+//     return arr;
+// }
+
+// Example usage
+// console.log(bubbleSort([34, 85, 65, 12, 11, 20, 80]));
+// console.log(bubbleSort([34, 12, 65, 12, 15]));
+
+/**
+[34, 85, 65, 12, 11, 20, 80]
+[34, 65, 12, 11, 20, 80, 85] after pass 1
+[34,12, 11, 20,65,80, 85] after pass 2
+[12,11, 20,34, 65,80, 85] after pass 3
+[11, 12, 20,34, 65,80, 85] after pass 4
+[11, 12, 20,34, 65,80, 85] after pass 5
+[11, 12, 20,34, 65,80, 85] after pass 6
+ **/
+
+//bubble sort?
+
+const sortBubble = (arr) => {
+    let arrlength = arr.length;
+
+    for (let i = 0; i < arrlength; i++) {
+        let swapped = false;
+        for (let j = 0; j < arrlength - i -1; j++) {
+            if (arr[j].length > arr[j + 1].length) {
+                // [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j+1] = temp;
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
+    } 
+    return arr;
 }
 
+// console.log(sortBubble(["dog", "banana", "panda", "rabbit", "bird", "monkeys"]));
+
+// console.log(sortBubble([34, 12, 65, 0, 15]));
 
 
+const add = (num1, num2) => num1 + num2;
 
-// loops
+// console.log(add(3,7)/2);
+// console.log(add(3,7)); 
+//can be called multiple times
 
-let i = 10;
+const average = (arr) => {
+    let sum = 0;
 
-while (i < 10) 
-{
-    i++;
-    console.log(i);
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    let average = sum/arr.length;
+    return average;
 }
-
-console.log(i);
+    
+console.log(average([2,4,4,6,4,8,2,10]));
